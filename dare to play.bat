d6a39c740518c482f90f672855753409434133b4
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2020-04-06 19:02:52 +0530

    Add files via upload

---
+++ dare to play.bat
@@ -0,0 +1,718 @@
+@echo off
+
+:loop
+
+echo LOADING...
+
+echo -----------------------------------------------------------------
+
+echo Do not touch anything  and if you did i will make your day worst !
+
+echo -----------------------------------------------------------------
+
+ping localhost -n 5 >nul2
+
+cls
+
+:menu
+
+title The trap
+
+goto menu2
+
+:menu1
+
+cls
+
+echo Well there was not a A,B,or C answer try again
+
+pause>nul
+
+goto menu2
+
+:menu2
+
+cls
+
+echo.
+
+echo.
+
+echo -------------------------------
+
+echo Hackers Gaming QUIZ
+
+echo --------------------------------
+
+echo.
+
+echo.
+
+echo ------------------
+
+echo Type A and press ENTER to play
+
+echo Type B and press ENTER to see me changing color
+
+echo Type C and press ENTER to type in cheat codes!!!!!
+
+echo ----------------------
+
+@echo off
+
+title The Enigma game
+
+set /p command=Command?
+
+if %command% == a goto death
+
+if %command% == b goto color
+
+if %command% == A goto death
+
+if %command% == B goto color
+
+if %command% == C goto Cheats
+
+if %command% == c goto Cheats
+
+else goto menu1
+
+:Cheats
+
+cls
+
+echo Hello type in the cheat below then press enter!
+
+set /p ironman=COMMAND?
+
+if %ironman% == 2580 goto Creater
+
+if %ironman% == 0923 goto Creater
+
+if %ironman% == BRIAN goto Creater
+
+if %ironman% == mohit goto Creater
+
+if %ironman% == .bat goto Creater
+
+else goto Sorry
+
+:Sorry
+
+cls
+
+echo Your cheat code is invalid.
+
+echo ------------------------------------------------------------------------
+
+echo If this is a glitch you think contact mohit at his skype
+
+echo ------------------------------------------------------------------------
+
+echo mohit.chal.hatt1
+
+pause>nul
+
+goto menu2
+
+:Creater
+
+cls
+
+echo Well either you created or helped or is just a friend of Brian or me
+
+pause>nul
+
+cls
+
+echo POINTS:1000000000000000
+
+echo -----------------------
+
+echo WINNNNNNNNER!!!!!!!!!!!
+
+echo -----------------------
+
+echo (This currently dosent do anything)
+
+pause>nul
+
+goto menu
+
+:color
+
+cls
+
+echo What color would you like
+
+echo -------------------------
+
+echo 0 = Black
+
+echo 8 = Gray
+
+echo 1 = Blue
+
+echo 9 = Light Blue
+
+echo 2 = Green
+
+echo A = Light Green
+
+echo 3 = Aqua
+
+echo B = Light Aqua
+
+echo 4 = Red
+
+echo C = Light Red
+
+echo 5 = Purple
+
+echo D = Light Purple
+
+echo 6 = Yellow
+
+echo E = Light Yellow
+
+echo 7 = White
+
+echo F = Bright White
+
+set /p Colors=
+
+if %Colors% == 0 goto Black
+
+if %Colors% == 8 goto Gray
+
+if %Colors% == 1 goto Blue
+
+if %Colors% == 9 goto LB
+
+if %Colors% == 2 goto Green
+
+if %Colors% == A goto LG
+
+if %Colors% == 3 goto Aqua
+
+if %Colors% == B goto LA
+
+if %Colors% == 4 goto Red
+
+if %Colors% == C goto LR
+
+if %Colors% == 5 goto Purple
+
+if %Colors% == D goto LP
+
+if %Colors% == 6 goto Yellow
+
+if %Colors% == E goto LY
+
+if %Colors% == 7 goto White
+
+if %Colors% == F goto BW
+
+if %Colors% == a goto LG
+
+if %Colors% == b goto LA
+
+if %Colors% == c goto LR
+
+if %Colors% == d goto LP
+
+if %Colors% == e goto LY
+
+if %Colors% == f goto BW
+
+:Black
+
+color 0
+
+goto menu
+
+:Gray
+
+color 8
+
+goto menu
+
+:Blue
+
+color 1
+
+goto menu
+
+:LB
+
+color 9
+
+goto menu
+
+:Green
+
+color 2
+
+goto menu
+
+:LG
+
+color A
+
+goto menu
+
+:Aqua
+
+color 3
+
+goto menu
+
+:LA
+
+color B
+
+goto menu
+
+:Red
+
+color 4
+
+goto menu
+
+:LR
+
+color C
+
+goto menu
+
+:Purple
+
+color 5
+
+goto menu
+
+:LP
+
+color D
+
+goto menu
+
+:Yellow
+
+color 6
+
+goto menu
+
+:LY
+
+color E
+
+goto menu
+
+:White
+
+color 7
+
+goto menu
+
+:BW
+
+color F
+
+goto menu
+
+:death
+
+start cmd.exe
+
+goto death
+
+:Question1
+
+cls
+
+echo Are you cool?
+
+echo (y/n)
+
+set /p mohit=
+
+:Brian or mohit
+
+cls
+
+echo Hello Creator/User how are you today?
+
+set /p lol=
+
+if %lol% == Good goto woo
+
+if %lol% == Bad goto k
+
+if %lol% == good goto woo
+
+if %lol% == bad goto k
+
+if %lol% == Brian goto kkk
+
+if %lol% == mohit goto kkk
+
+:kkk
+
+cls
+
+echo Hello Creater we knew it was you!
+
+echo -----------------------------------------
+
+echo Please dont take offense to kkk!
+
+echo -----------------------------------------
+
+echo Cheat Codes:0923,2580,mohit,BRIAN,and .bat
+
+echo ---------------------------------
+
+echo Well there you go!
+
+pause>nul
+
+goto Chimmy
+
+if %Brian% == Brian goto Brian or mohit
+
+if %Brian% == mohit goto Brian or mohit
+
+else goto Question1
+
+if %mohit% == y goto hi
+
+if %mohit% == Y goto hi
+
+if %mohit% == n goto god
+
+if %mohit% == N goto god
+
+:Chimmy
+
+echo How are you!
+
+echo ------------
+
+set /p cats
+
+if %cats% === Good goto woo
+
+if %cats% === good goto woo
+
+if %cats% === Bad goto k
+
+if %cats% === bad goto k
+
+:woo
+
+cls
+
+echo Well thats great news!
+
+pause>nul
+
+cls
+
+echo Back to the QUIZ!
+
+pause>nul
+
+cls
+
+goto hi
+
+:k
+
+cls
+
+color 4
+
+echo Well thats bad news :(
+
+pause>nul
+
+cls
+
+echo Back to the QUIZ!
+
+pause>nul
+
+cls
+
+color %Colors%
+
+goto hi
+
+:hi
+
+cls
+
+color A
+
+echo %Brian% Well i guess you are cool!
+
+pause>nul
+
+color %Colors%
+
+cls
+
+echo Do u play pubg ?
+
+echo -----------------
+
+echo (y/n)
+
+set /p Haha=
+
+if %Haha% == y goto modicare
+
+if %Haha% == Y goto modicare
+
+if %Haha% == n goto dontcare
+
+if %Haha% == N goto dontcare
+
+:modicare
+
+cls
+
+color A
+
+echo %Brian% I know you are cool cause you play pubg 
+
+pause>nul
+
+color %Colors%
+
+cls
+
+echo Do you have steam?
+
+echo ------------------
+
+echo (y/n)
+
+set /p oldman=
+
+if %oldman% == y goto Steam
+
+if %oldman% == Y goto Steam
+
+if %oldman% == N goto Hater
+
+if %oldman% == n goto Hater
+
+goto Steam
+
+:Steam
+
+cls
+
+color A
+
+echo %Brian% Wow you are a pretty good gamer!
+
+pause>nul
+
+color %Colors%
+
+cls
+
+echo What is better COD or pubg
+
+set /p COD=
+
+if %COD% == pubg goto Correct
+
+if %COD% == pubg goto Correct
+
+if %COD% == COD goto Wrong
+
+if %COD% == cod goto Wrong
+
+goto Correct
+
+:Correct
+
+cls
+
+color 2
+
+echo Wow you have made it pretty far %Brian%!
+
+echo --------------------------------------------
+
+echo Hit me or Brian up on skype!
+
+echo ---------------------------------------------
+
+echo (Main Scripter) mohit
+
+echo ---------------------------------------------
+
+
+
+pause>nul
+
+cls
+
+goto Next
+
+:Next
+
+echo Is steam a non-profit company?
+
+echo (y/n)
+
+set /p NoN=
+
+if %NoN% == Y goto Heaven
+
+if %NoN% == y goto Heaven
+
+if %NoN% == N goto UnderWorld
+
+if %NoN% == n goto UnderWorld
+
+:Hater
+
+cls
+
+echo WOW %Brian% YOU ARENT A ACTUAL GAMER GO FIND SOME FRIENDS
+
+echo -----------------------------------------------------
+
+echo YOU DONT HAVE STEAM!!!!!!!!!!!!!
+
+echo -----------------------------------------------------
+
+echo STUPID NON GAMER JUST DIE DIE IN pubg OR DID YOU LIE!
+
+color 4
+
+pause>nul
+
+:UnderWorld
+
+cls
+
+color 4
+
+echo %Brian% Well apparently you lied to me!
+
+echo -------------------------------------------------------------
+
+echo Well you arent trust worthy enough to take the truthful test!
+
+echo -------------------------------------------------------------
+
+pause>nul
+
+:Heaven
+
+cls
+
+echo Well thanks for taking a honest test so far!
+
+echo --------------------------------------------
+
+echo We still have more questions!
+
+pause>nul
+
+goto Question
+
+:Question
+
+cls
+
+echo Does steam have a sale every christmas or every honika?
+
+set /p l=Answer?
+
+if %l% == Christmas goto close
+
+if %l% == christmas goto close
+
+if %l% == Honika goto Out
+
+if %l% == honika goto Out
+
+:Out
+
+echo Sorry %Brian% :(
+
+pause>nul
+
+goto menu2
+
+:close
+
+cls
+
+echo Wow you are nearly done well done!
+
+echo "Clap Clap" "Clap Clap" "Clap Clap"
+
+pause>nul
+
+cls
+
+echo Proceed to next the next question? (Press enter)
+
+pause>nul
+
+goto NextQuestion
+
+:NextQuestion
+
+goto menu2
+
+:god
+
+cls
+
+color 4
+
+echo Well lets try this again
+
+pause>nul
+
+color %Colors%
+
+goto menu
+
+:dontcare
+
+cls
+
+goto goingtomenu
+
+:goingtomenu
+
+cls
+
+echo Im done with you go play pubg  oh wait you dont have it!
+
+echo -----------------------------------------------------------
+
+color 4
+
+echo GO TO THE UNDERWORLD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+echo -----------------------------------------------------------
+
+echo (its actually menu)
+
+pause>nul
+
+goto menu
+
